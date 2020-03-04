6ebc478c1ae211f149aca5986e28854427ee2720
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-03-04 17:19:25 +0000

    version 1.1.0

--- routes/router.js
+++ routes/router.js
@@ -1,16 +1,16 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const router = Router();
 
 //import weather.js function
 //returns a promise of type any - means you have to 'await'
 const getWeather = require('../lib/getWeather')
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     res.render('index');
 });
 
 
-router.get('/getWeather', async (req,res) => {
+router.get('/getWeather', async (req, res) => {
     let data = await getWeather.getWeather()
     console.log(data)
     res.render('getWeather', {
@@ -19,32 +19,36 @@ router.get('/getWeather', async (req,res) => {
     })
 })
 
-router.get('/harryPotter', async (req,res) => {
+router.get('/harryPotter', async (req, res) => {
     let data = await getWeather.getHarryPotter()
     console.log(data)
     res.render('harryPotter', {
         data,
-        title:`You have been randomy sorted into ${data}`
+        title: `You have been randomy sorted into ${data}`
     })
 })
 
 
-router.get('/getNasaImage', async (req,res) => {
+router.get('/getNasaImage', async (req, res) => {
     let data = await getWeather.getNasaImage()
     // let date = data.date,
-    let image = data.url
+    let date = data.date;
+    let explanation = data.explanation;
+    let url = data.url;
     // let explanation = data.explanation
     console.log(data)
     res.render('getNasaImage', {
-        image
+        date: date,
+        explanation: explanation,
+        url: url
     })
 })
 
-router.post('/', async(req,res) => { // here you display the information
+router.post('/', async (req, res) => { // here you display the information
     let city = req.body.city;
     let countryCode = req.body.countryCode;
-// dont want all logic so can use lib folder incase want to use the code elsewhere
-    let data = await getWeather.getWeather(city,countryCode);
+    // dont want all logic so can use lib folder incase want to use the code elsewhere
+    let data = await getWeather.getWeather(city, countryCode);
 
     let weatherData = {
         main: data.weather[0].main,
@@ -54,12 +58,12 @@ router.post('/', async(req,res) => { // here you display the information
     }
     let icon = data.weather[0].icon;
     console.log(data);
-    
-    res.render('index', {weatherData, icon});
+
+    res.render('index', { weatherData, icon });
     //check on imsonia the get data works before build the front end
 });
 
-router.get('/chuckNorris', async(req,res) => {
+router.get('/chuckNorris', async (req, res) => {
     let data = await getWeather.chuckNorris()
     data = JSON.parse(data)
     console.log(data.value)
@@ -69,29 +73,46 @@ router.get('/chuckNorris', async(req,res) => {
         data,
         joke: joke
     });
-    });
+});
 
-router.post('/chuckNorris', async(req,res) => {
+router.post('/chuckNorris', async (req, res) => {
     let data = await getWeather.chuckNorris()
     console.log(data)
     let joke = data.categories.value;
-    res.render('chuckNorris', {data: {joke}});
+    res.render('chuckNorris', { data: { joke } });
 })
 
-router.get('/cocktailInfo', async(req,res) => {
+router.get('/cocktailInfo', async (req, res) => {
     let data = await getWeather.cocktailInfo()
     console.log(data)
+    // console.log(data.strDrink)
+    // console.log(data.strDrinkThumb)
+
+    // let strDrink = data.strDrink;
+    // let strDrinkThumb = data.strDrinkThumb;
     res.render('cocktailInfo', {
         data,
-        title:`Your cocktail is ${data}`
-    });
+        title: `You have been given cocktail ${data}`
+    }); 
+        // strDrink: strDrink,
+        // strDrinkThumb: strDrinkThumb
+        // data,
+        // str: strDrink,
+        // strDrinkThumb: strDrinkThumb
+        // title: `Your cocktail is ${data}`
+    // });
 });
 
-router.post('/cocktailInfo', async(req,res) => {
+router.post('/cocktailInfo', async (req, res) => {
     let ingredient = req.body.ingredient;
+    console.log(ingredient)
     let data = await getWeather.cocktailInfo(ingredient);
-    res.render('/cocktailInfo', {ingredient})
-})
+
+    let strDrink = data.drinks.strDrink;
+    console.log(strDrink);
+    let strDrinkThumb = data.drinks.strDrinkThumb;
+    res.render('cocktailInfo', {strDrink,strDrinkThumb});
+        });
 
 
 // get method needed for cocktail... 
@@ -103,18 +124,15 @@ router.post('/cocktailInfo', async(req,res) => {
 //     let data = await cocktailInfo(ingredient);
 
 //     let strDescription = data.strDescription;
-    
+
 //     res.render('cocktailInfo', {strDescription});
 // })
 
-router.post('/getNasaImage', async(req,res) => {
-    let data = await getWeather.getNasaImage()
-    console.log(data)
-    res.render('getNasaImage', {
-        data,
-        title: `Picture of the day ${data}`
-    })
-})
+// router.post('/getNasaImage', async(req,res) => {
+//     let data = await getWeather.getNasaImage()
+//     console.log(data)
+//     res.render('getNasaImage', {data: {date,explanation,url}});
+// })
 
 // all routes go in here
 
